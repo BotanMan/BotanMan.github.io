9c13b5109602a227cd6704a017df10dd727b904e
Igor <Boky>
2014-06-10 02:18:27 +0400

    Added beta prototype version

---
+++ app/app.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+var app = angular.module('app', [
+  'ngRoute',
+  'ngAnimate',
+  'ngSanitize',
+  'ui.router',
+  'fastClick'
+])
+
+    .run(
+      ['$rootScope', '$state', '$stateParams','$http', 
+          function ($rootScope, $state, $stateParams, $http) {
+              $http.defaults.useXDomain = true;
+		      //transclude state and stateParams to $rootScope
+              $rootScope.state = $state;
+              $rootScope.stateParams = $stateParams;
+          }]);
+
+
+angular.module('fastClick', []).
+run(function () {
+    FastClick.attach(document.body);
+});
---
+++ app/common/routes.js
@@ -0,0 +1,32 @@
+﻿app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        // login prototype
+        //.state('login', {
+        //    url: "/login",
+        //    templateUrl: 'app/login/login.cshtml',
+        //    controller: 'loginCtrl'
+        //})
+        // root
+        .state('root', {
+            "abstract": true,
+            templateUrl: 'app/layout/layout.html',
+        })
+        // address book
+        .state('root.addressBook', {
+            url: "/address-book",
+            templateUrl: 'app/address-book/address-book.html'
+        })
+        // main
+        .state('root.main', {
+            url: "/main",
+            templateUrl: 'app/main/main.html'
+        })
+        // bizTalk dashboard
+        .state('root.profile', {
+            url: "/profile",
+            templateUrl: 'app/profile/profile.html'
+        })
+
+    $urlRouterProvider
+         .otherwise('/main');
+}]);
